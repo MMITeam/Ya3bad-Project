80b73516c8ab6ec96f921110558d01b21eb2944e
mahmoud <mahmabubaker@gmail.com>
2014-05-23 12:24:28 +0300

    update my controller and all controllers  functions

---
+++ application/controllers/admin/Menu.php
@@ -0,0 +1,58 @@
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+class Menu extends MY_Controller {
+
+	public function __construct() {
+		parent::__construct();
+			
+
+	}
+
+	public function index() {
+$c = $this -> uri -> rsegment(1) ;
+		echo "you are in  ".$c." index";
+	}
+
+	public function save() {
+		$fields = array("title", "image");
+		parent::save($fields);
+
+	}
+
+	public function delete() {
+		parent::delete();
+
+	}
+
+	public function get_by() {
+
+		$fields = array("id");
+		parent::get_by($fields);
+	}
+
+	public function search_by() {
+
+		$fields = array("title");
+		parent::get_by($fields);
+	}
+
+	public function get_by_pagination() {
+		$fields = array("title");
+		parent::get_by($fields);
+
+	}
+
+	public function approve() {
+		parent::approve();
+
+	}
+
+	public function archive() {
+		parent::archive();
+
+	}
+
+}
+?>
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,8 +1,70 @@
 <?php
-class MY_Controller extends CI_Controller
-{
-   function __construct()
-   {
-      parent::__construct();
-   }
+class MY_Controller extends CI_Controller {
+	function __construct() {
+		parent::__construct();
+		$c = $this -> uri -> rsegment(1) . "_model";
+		
+		$this -> load -> model('admin/'.$c);
+	}
+
+
+	public function save($fields) {
+$c = $this -> uri -> rsegment(1) . "_model";
+
+		$data = $this -> $c -> array_from_post($fields);
+		
+		$this -> $c -> save($data);
+
+	}
+
+	public function delete() {
+		$id = $this -> input -> post("id");
+		$c = $this -> uri -> rsegment(1) . "_model";
+
+		$this -> $c -> delete(24);
+
+	}
+
+	public function get() {
+
+	}
+
+	public function get_by($fields, $signal = FALSE, $perpage = 1, $start = 0) {
+		$c = $this -> uri -> rsegment(1) . "_model";
+		$where = $this -> $c -> array_from_post($fields);
+
+		return $this -> $c -> get_by($where, $signal, $perpage, $start);
+	}
+
+	public function search_by($fields, $signal = FALSE, $perpage = 1, $start = 0) {
+		$c = $this -> uri -> rsegment(1) . "_model";
+		$where = $this -> $c -> array_from_post($fields);
+
+		return $this -> $c -> search_by($where, $signal, $perpage, $start);
+	}
+
+	public function get_by_pagination($fields, $signal = FALSE, $perpage = 1, $start = 0) {
+
+		$c = $this -> uri -> rsegment(1) . "_model";
+		$where = $this -> $c -> array_from_post($fields);
+
+		return get_by_pagination($where, $signal, $perpage, $start);
+	}
+
+	public function approve() {
+		$c = $this -> uri -> rsegment(1) . "_model";
+		$id = $this -> input -> post('id');
+		$data = array('status' => 'approved');
+		$this -> $c -> save($data, $id);
+
+	}
+
+	public function archive() {
+		$c = $this -> uri -> rsegment(1) . "_model";
+		$id = $this -> input -> post('id');
+		$data = array('status' => 'archive');
+		$this -> $c -> save($data, $id);
+
+	}
+
 }
