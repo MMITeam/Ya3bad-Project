d5c66c6d369730250e53877a04875f28a27cd91a
unknown <Haneen@.(none)>
2014-06-01 23:31:03 +0300

    add the main html container for the datails view and the vedio view and make the needed css changes to implement the mock-up requierment

---
+++ application/views/details.php
@@ -0,0 +1,124 @@
+<div id="main_area" class="main_details_continater">
+
+	<div id="side-container">
+
+		<div class="sub_main_news block_num_13">
+			test
+		</div>
+		<div class="sub_main_news block_num_13">
+			test
+		</div>
+		<div class="sub_main_news block_num_13">
+			test
+		</div>
+
+		<div class="simple_wattan_block block_num_13" >
+			<div class="bh">
+				<div>
+					<a href="http://www.wattan.tv/ar/news/blog"> أقلام وآراء </a>
+				</div><span></span>
+			</div>
+			<div class="bc">
+				<div class="block_41_container blocklist_img_title">
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data3.wattan.tv/image/70X40/00056020381230484415821186567011.jpg" title="الرئيس ... وقتل المرأة .... وأدوات التغيير" alt="الرئيس ... وقتل المرأة .... وأدوات التغيير" width="70" height="40"></span>الرئيس ... وقتل المرأة .... وأدوات التغيير</span> <span class="clear"></span> </span> </a>
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data1.wattan.tv/image/70X40/04092322602070070200185066638213.jpg" title="الفرز الحاد - بقلم: حمدي فراج" alt="الفرز الحاد - بقلم: حمدي فراج" width="70" height="40"></span>الفرز الحاد - بقلم: حمدي فراج</span> <span class="clear"></span> </span> </a>
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data3.wattan.tv/image/70X40/00056020381230484415821186567011.jpg" title="الرئيس ... وقتل المرأة .... وأدوات التغيير" alt="الرئيس ... وقتل المرأة .... وأدوات التغيير" width="70" height="40"></span>الرئيس ... وقتل المرأة .... وأدوات التغيير</span> <span class="clear"></span> </span> </a>
+
+				</div>
+				<div class="clear"></div>
+
+			</div>
+
+		</div>
+
+		<div class="simple_wattan_block block_num_13">
+			<div class="bh">
+				<div>
+					<a href="http://www.wattan.tv/ar/news/blog">عين على يعبد </a>
+				</div><span></span>
+			</div>
+			<div class="bc">
+				<div class="block_41_container blocklist_img_title">
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data3.wattan.tv/image/70X40/00056020381230484415821186567011.jpg" title="الرئيس ... وقتل المرأة .... وأدوات التغيير" alt="الرئيس ... وقتل المرأة .... وأدوات التغيير" width="70" height="40"></span>الرئيس ... وقتل المرأة .... وأدوات التغيير</span> <span class="clear"></span> </span> </a>
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data1.wattan.tv/image/70X40/04092322602070070200185066638213.jpg" title="الفرز الحاد - بقلم: حمدي فراج" alt="الفرز الحاد - بقلم: حمدي فراج" width="70" height="40"></span>الفرز الحاد - بقلم: حمدي فراج</span> <span class="clear"></span> </span> </a>
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data3.wattan.tv/image/70X40/00056020381230484415821186567011.jpg" title="الرئيس ... وقتل المرأة .... وأدوات التغيير" alt="الرئيس ... وقتل المرأة .... وأدوات التغيير" width="70" height="40"></span>الرئيس ... وقتل المرأة .... وأدوات التغيير</span> <span class="clear"></span> </span> </a>
+
+				</div>
+				<div class="clear"></div>
+
+			</div>
+
+		</div>
+
+		<div class="simple_wattan_block block_num_13">
+			<div class="bh">
+				<div>
+					<a href="http://www.wattan.tv/ar/news/blog">الصحافة العبرية</a>
+				</div><span></span>
+			</div>
+			<div class="bc">
+				<div class="block_41_container blocklist_img_title">
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data3.wattan.tv/image/70X40/00056020381230484415821186567011.jpg" title="الرئيس ... وقتل المرأة .... وأدوات التغيير" alt="الرئيس ... وقتل المرأة .... وأدوات التغيير" width="70" height="40"></span>الرئيس ... وقتل المرأة .... وأدوات التغيير</span> <span class="clear"></span> </span> </a>
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data1.wattan.tv/image/70X40/04092322602070070200185066638213.jpg" title="الفرز الحاد - بقلم: حمدي فراج" alt="الفرز الحاد - بقلم: حمدي فراج" width="70" height="40"></span>الفرز الحاد - بقلم: حمدي فراج</span> <span class="clear"></span> </span> </a>
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data3.wattan.tv/image/70X40/00056020381230484415821186567011.jpg" title="الرئيس ... وقتل المرأة .... وأدوات التغيير" alt="الرئيس ... وقتل المرأة .... وأدوات التغيير" width="70" height="40"></span>الرئيس ... وقتل المرأة .... وأدوات التغيير</span> <span class="clear"></span> </span> </a>
+
+				</div>
+				<div class="clear"></div>
+
+			</div>
+
+		</div>
+
+		<div class="simple_wattan_block block_num_13">
+			<div class="bh">
+				<div>
+					<a href="http://www.wattan.tv/ar/news/blog">الطقس</a>
+				</div><span></span>
+			</div>
+			<div class="bc">
+				<div class="block_41_container blocklist_img_title">
+					//we
+
+				</div>
+				<div class="clear"></div>
+
+			</div>
+
+		</div>
+		<div class="simple_wattan_block block_num_13">
+			<div class="bh">
+				<div>
+					<a href="http://www.wattan.tv/ar/news/blog">الأكثر قرائة</a>
+				</div><span></span>
+			</div>
+			<div class="bc">
+				<div class="block_41_container blocklist_img_title">
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data3.wattan.tv/image/70X40/00056020381230484415821186567011.jpg" title="الرئيس ... وقتل المرأة .... وأدوات التغيير" alt="الرئيس ... وقتل المرأة .... وأدوات التغيير" width="70" height="40"></span>الرئيس ... وقتل المرأة .... وأدوات التغيير</span> <span class="clear"></span> </span> </a>
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data1.wattan.tv/image/70X40/04092322602070070200185066638213.jpg" title="الفرز الحاد - بقلم: حمدي فراج" alt="الفرز الحاد - بقلم: حمدي فراج" width="70" height="40"></span>الفرز الحاد - بقلم: حمدي فراج</span> <span class="clear"></span> </span> </a>
+					<a href="#"> <span class="bit_frame block_41_bit"> <span class="t"><span class="m"><img src="http://data3.wattan.tv/image/70X40/00056020381230484415821186567011.jpg" title="الرئيس ... وقتل المرأة .... وأدوات التغيير" alt="الرئيس ... وقتل المرأة .... وأدوات التغيير" width="70" height="40"></span>الرئيس ... وقتل المرأة .... وأدوات التغيير</span> <span class="clear"></span> </span> </a>
+
+				</div>
+				<div class="clear"></div>
+
+			</div>
+
+		</div>
+
+	</div>
+	<div id="main-container">
+
+		<div class="main_block cat_block" >
+			<div class="bh">
+				<div>
+					<a href="">فلسطيني</a>
+				</div><span></span>
+			</div>
+			<div class="bc"></div>
+		</div>
+
+	</div>
+</div>
+</div>
+</body>
+
+</html>
