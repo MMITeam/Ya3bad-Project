2977c585f83200ad3e8f225131f2e2dce56e260a
unknown <Jon@.(none)>
2014-06-20 13:38:15 +0300

    h

--- application/assets/yabad.sql
+++ application/assets/yabad.sql
@@ -1,20 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 2.11.11.3
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jun 13, 2014 at 07:09 AM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.1
+-- Host: 166.62.8.16
+-- Generation Time: Jun 20, 2014 at 04:37 AM
+-- Server version: 5.5.33
+-- PHP Version: 5.1.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `yabad`
@@ -26,7 +19,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `ads`
 --
 
-CREATE TABLE IF NOT EXISTS `ads` (
+CREATE TABLE `ads` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `type` int(11) NOT NULL,
@@ -43,8 +36,7 @@ CREATE TABLE IF NOT EXISTS `ads` (
 -- Dumping data for table `ads`
 --
 
-INSERT INTO `ads` (`id`, `title`, `type`, `adsfile`, `user`, `pos`, `created`, `modified`, `status`) VALUES
-(1, '0', 0, '', 1, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'pennding');
+INSERT INTO `ads` VALUES(1, '0', 0, '', 1, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'pennding');
 
 -- --------------------------------------------------------
 
@@ -52,7 +44,7 @@ INSERT INTO `ads` (`id`, `title`, `type`, `adsfile`, `user`, `pos`, `created`, `
 -- Table structure for table `category`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
+CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
@@ -66,20 +58,19 @@ CREATE TABLE IF NOT EXISTS `category` (
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`id`, `title`, `created`, `modified`, `user`, `status`) VALUES
-(1, 'فلسطيني', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(2, 'عربي  و دولي', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(3, 'رياضة', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(4, 'اقتصاد', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(5, 'علوم وتكنولوجيا', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(6, 'فن و ثقافة', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(7, 'الصحة والبيئة', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(8, 'منوعات', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(9, 'أقلام وآراء', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(10, 'عين على يعبد', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(11, 'الصحافة العبرية', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved'),
-(12, 'الأكثر قرائة', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'block'),
-(13, 'اخبار سوريا', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(1, 'فلسطيني', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(2, 'عربي  و دولي', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(3, 'رياضة', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(4, 'اقتصاد', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(5, 'علوم وتكنولوجيا', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(6, 'فن و ثقافة', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(7, 'الصحة والبيئة', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(8, 'منوعات', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(9, 'أقلام وآراء', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(10, 'عين على يعبد', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(11, 'الصحافة العبرية', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `category` VALUES(12, 'الأكثر قرائة', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'block');
+INSERT INTO `category` VALUES(13, 'اخبار سوريا', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
 
 -- --------------------------------------------------------
 
@@ -87,7 +78,7 @@ INSERT INTO `category` (`id`, `title`, `created`, `modified`, `user`, `status`) 
 -- Table structure for table `ci_sessions`
 --
 
-CREATE TABLE IF NOT EXISTS `ci_sessions` (
+CREATE TABLE `ci_sessions` (
   `session_id` varchar(40) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   `ip_address` varchar(45) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   `user_agent` varchar(120) COLLATE utf8_unicode_ci NOT NULL,
@@ -101,13 +92,13 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 -- Dumping data for table `ci_sessions`
 --
 
-INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('2ef111f4d9288a616e6c20f467e7ba47', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401735608, ''),
-('4b8a3abf60993e846b7b6bd09ee3fb15', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401735607, 'a:2:{s:9:"user_data";s:0:"";s:2:"id";s:1:"3";}'),
-('925fab4624e9d6333dd4e1c4def805e5', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401743105, 'a:5:{s:9:"user_data";s:0:"";s:2:"id";s:1:"1";s:8:"username";s:4:"emad";s:9:"validated";b:1;s:13:"flash:new:msg";s:23:"Record Saved Succefully";}'),
-('b59112363cb004bf58f28ee8d483f980', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401735605, ''),
-('cbb657fb2d1d28a3bee75296360386a5', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401735607, ''),
-('e2d3c27fc0a11e38af0a8a0b4bd41230', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401737900, 'a:2:{s:9:"user_data";s:0:"";s:2:"id";s:1:"3";}');
+INSERT INTO `ci_sessions` VALUES('2ef111f4d9288a616e6c20f467e7ba47', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401735608, '');
+INSERT INTO `ci_sessions` VALUES('4b8a3abf60993e846b7b6bd09ee3fb15', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401735607, 'a:2:{s:9:"user_data";s:0:"";s:2:"id";s:1:"3";}');
+INSERT INTO `ci_sessions` VALUES('84f78bc3fba8d6f6bd881f993941adbf', '5.43.219.229', 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36', 1403248268, 'a:8:{s:9:"user_data";s:0:"";s:2:"id";s:1:"1";s:8:"username";s:4:"emad";s:9:"validated";b:1;s:4:"type";s:0:"";s:4:"name";s:4:"emad";s:3:"msg";s:25:"Record Updated Succefully";s:9:"error_msg";s:33:"Record did not Deleted Succefully";}');
+INSERT INTO `ci_sessions` VALUES('925fab4624e9d6333dd4e1c4def805e5', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401743105, 'a:5:{s:9:"user_data";s:0:"";s:2:"id";s:1:"1";s:8:"username";s:4:"emad";s:9:"validated";b:1;s:13:"flash:new:msg";s:23:"Record Saved Succefully";}');
+INSERT INTO `ci_sessions` VALUES('b59112363cb004bf58f28ee8d483f980', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401735605, '');
+INSERT INTO `ci_sessions` VALUES('cbb657fb2d1d28a3bee75296360386a5', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401735607, '');
+INSERT INTO `ci_sessions` VALUES('e2d3c27fc0a11e38af0a8a0b4bd41230', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.41 Safari/537.36', 1401737900, 'a:2:{s:9:"user_data";s:0:"";s:2:"id";s:1:"3";}');
 
 -- --------------------------------------------------------
 
@@ -115,16 +106,22 @@ INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activ
 -- Table structure for table `media`
 --
 
-CREATE TABLE IF NOT EXISTS `media` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `media` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `news_id` int(11) NOT NULL,
   `path` text COLLATE utf8_unicode_ci NOT NULL,
   `type` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   `user` int(11) NOT NULL,
-  `status` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `status` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `media`
+--
+
 
 -- --------------------------------------------------------
 
@@ -132,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `media` (
 -- Table structure for table `menu`
 --
 
-CREATE TABLE IF NOT EXISTS `menu` (
+CREATE TABLE `menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` text COLLATE utf8_unicode_ci NOT NULL,
   `link` text COLLATE utf8_unicode_ci NOT NULL,
@@ -147,17 +144,15 @@ CREATE TABLE IF NOT EXISTS `menu` (
 -- Dumping data for table `menu`
 --
 
-INSERT INTO `menu` (`id`, `text`, `link`, `created`, `modified`, `user`, `status`) VALUES
-(1, 'الرئيسية', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 3, ''),
-(2, 'فلسطيني', 'home/lists/1', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 'approved'),
-(3, 'دولي', 'home/lists/2', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, ''),
-(4, 'رياضة', 'home/lists/3', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, ''),
-(5, 'اقتصاد', 'home/lists/4', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, ''),
-(6, 'علوم وتكنولوجيا', 'home/lists/5', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, ''),
-(7, 'فن وثقافة', 'home/lists/6', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, ''),
-(8, 'الصحة والبيئة', 'home/lists/7', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, ''),
-(9, 'منوعات', 'home/lists/8', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, ''),
-(10, 'محمود فيس', 'http://www.facebook.com/mahmabubaker', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'pennding');
+INSERT INTO `menu` VALUES(1, 'الرئيسية', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 3, '');
+INSERT INTO `menu` VALUES(2, 'فلسطيني', 'home/lists/1', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 'approved');
+INSERT INTO `menu` VALUES(3, 'دولي', 'home/lists/2', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `menu` VALUES(4, 'رياضة', 'home/lists/3', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `menu` VALUES(5, 'اقتصاد', 'home/lists/4', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `menu` VALUES(6, 'علوم وتكنولوجيا', 'home/lists/5', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `menu` VALUES(7, 'فن وثقافة', 'home/lists/6', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `menu` VALUES(8, 'الصحة والبيئة', 'home/lists/7', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
+INSERT INTO `menu` VALUES(9, 'منوعات', 'home/lists/8', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'approved');
 
 -- --------------------------------------------------------
 
@@ -165,7 +160,7 @@ INSERT INTO `menu` (`id`, `text`, `link`, `created`, `modified`, `user`, `status
 -- Table structure for table `news`
 --
 
-CREATE TABLE IF NOT EXISTS `news` (
+CREATE TABLE `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `shortDesc` text COLLATE utf8_unicode_ci NOT NULL,
   `fullDesc` text COLLATE utf8_unicode_ci NOT NULL,
@@ -178,7 +173,12 @@ CREATE TABLE IF NOT EXISTS `news` (
   `visits` int(11) NOT NULL,
   `pro` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=175 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=181 ;
+
+--
+-- Dumping data for table `news`
+--
+
 
 -- --------------------------------------------------------
 
@@ -186,14 +186,30 @@ CREATE TABLE IF NOT EXISTS `news` (
 -- Table structure for table `slider`
 --
 
-CREATE TABLE IF NOT EXISTS `slider` (
+CREATE TABLE `slider` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `news_id` int(11) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `status` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` VALUES(24, 175, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(25, 176, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(26, 177, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(27, 178, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(28, 180, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(29, 176, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(30, 178, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(31, 178, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(32, 178, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(33, 177, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
+INSERT INTO `slider` VALUES(34, 177, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'approved');
 
 -- --------------------------------------------------------
 
@@ -201,7 +217,7 @@ CREATE TABLE IF NOT EXISTS `slider` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(150) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `phone` text COLLATE utf8_unicode_ci NOT NULL,
@@ -222,10 +238,5 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `phone`, `username`, `password`, `email`, `type`, `created`, `modified`, `status`, `user`) VALUES
-(1, 'emad', '0597720001', 'emad', 'emademad', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 0),
-(2, '', '059999', 'oday', 'odayoday', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 0);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `users` VALUES(1, 'emad', '0597720001', 'emad', 'emademad', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 0);
+INSERT INTO `users` VALUES(2, '', '059999', 'oday', 'odayoday', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 0);
