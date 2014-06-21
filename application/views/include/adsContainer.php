5fd5de01a79de4b66193ed47e65622162c38bf00
issamshadid <shadeed.1990@gmail.com>
2014-06-21 14:24:23 +0300

    s

---
+++ application/views/include/adsContainer.php
@@ -0,0 +1,21 @@
+	<div id="sub_main_news_container">
+		<div class="sub_main_news">
+			<object id="obj178" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" border="0" width="300" height="90">
+	<param name="movie" value="http://www.wattan.tv/data/banners/be_178.swf?clickTAG=http://">
+	<param name="quality" value="High">
+	<embed src="http://www.wattan.tv/data/banners/be_178.swf?clickTAG=http://" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="obj178" width="300" height="90"></object>
+		</div>
+		<div class="sub_main_news">
+		<object id="obj154" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" border="0" width="300" height="90">
+	<param name="movie" value="http://www.wattan.tv/data/banners/be_154.swf?clickTAG=http://">
+	<param name="quality" value="High">
+	<embed src="http://www.wattan.tv/data/banners/be_154.swf?clickTAG=http://" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="obj154" width="300" height="90"></object>
+		</div>
+		<div class="sub_main_news">
+		<object id="obj176" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" border="0" width="300" height="90">
+	<param name="movie" value="http://www.wattan.tv/data/banners/be_176.swf?clickTAG=http://">
+	<param name="quality" value="High">
+	<embed src="http://www.wattan.tv/data/banners/be_176.swf?clickTAG=http://" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="obj176" width="300" height="90"></object>
+	</div>
+	</div>
+	
