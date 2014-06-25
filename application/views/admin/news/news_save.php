6a54aadbbccce3bfbb1333cf3dade0743bf6c03c
unknown <Jon@.(none)>
2014-06-25 23:25:25 +0300

    dfd

--- application/views/admin/news/news_save.php
+++ application/views/admin/news/news_save.php
@@ -69,7 +69,33 @@
 </div>
 
 
-<textarea  class="ckeditor" id = "editor1" name  =  "fullDesc" >
+<script type="text/javascript" src="<?php  echo HTTP_JS_PATH; ?>tinymce.min.js"></script>
+<script type="text/javascript">
+tinymce.init({
+    selector: "textarea",
+    theme: "modern",
+    plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor colorpicker textpattern"
+    ],
+         file_browser_callback: function(field_name, url, type, win) {
+            if(type=='image') $('#my_form input').click();
+        },
+    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    toolbar2: "print preview media | forecolor backcolor emoticons",
+    image_advtab: true,
+    templates: [
+        {title: 'Test template 1', content: 'Test 1'},
+        {title: 'Test template 2', content: 'Test 2'}
+    ]
+});
+</script>
+
+
+    <textarea name="fullDesc" style="width:80%;height:300px;" ></textarea>
+
 <?php $val  =  isset(${$c}) && !empty(${$c}) ?   ${$c}->fullDesc : ""  ;echo $val; ?>
 	
 </textarea>
